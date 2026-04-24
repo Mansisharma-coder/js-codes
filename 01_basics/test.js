cc7f9b61120acf236901b3aa390d314735fc6633
Mansisharma-coder <mgopiath@gmail.com>
2026-04-24 11:11:57 +0000

    new files

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
+
 console.log("hello world")
+// alert("hello world")
+let name = "john"
+let age = 30;
+// let isloggedIn = false
+// let state;
+console.table([name, age])
+console.table([typeof name, typeof age])
